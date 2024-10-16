b6b217abef1d5b695f5736302205ff364faaa759
Thais Canguçu <160797281+Thaiscangucu@users.noreply.github.com>
2024-10-16 16:43:15 -0300

    Create score.py

---
+++ SnakeGame/score.py
@@ -0,0 +1,33 @@
+
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("white")
+        self.penup()  # Fixed from pu() to penup()
+        self.goto(x=0, y=270)
+        self.update_score()
+        self.hideturtle()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase(self):
+        self.score += 1
+        self.update_score()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()  # Ensure the score is reset visually
